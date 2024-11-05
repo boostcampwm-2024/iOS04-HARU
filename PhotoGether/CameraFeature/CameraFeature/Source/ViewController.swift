7cc48e8fd147b8875b4d9639c4a5265735a5e619
Kiyoung <rlaky45@naver.com>
2024-11-05 22:01:04 +0900

    chore :: 프로젝트 데모 세팅
    카페 시간이 없어요
    울어라 지옥참마도

---
+++ PhotoGether/CameraFeature/CameraFeature/Source/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+public class ViewController: UIViewController {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
