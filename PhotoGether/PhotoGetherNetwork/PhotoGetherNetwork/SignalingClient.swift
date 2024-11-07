9639ab0faf7711e57989c1441a00b1b76498e812
0Hooni <thddudgns972@gmail.com>
2024-11-07 17:00:49 +0900

    chore :: SignalingClient 구현
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>
    Co-Authored-By: seuhong <66902876+hsw1920@users.noreply.github.com>
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>

---
+++ PhotoGether/PhotoGetherNetwork/PhotoGetherNetwork/SignalingClient.swift
@@ -0,0 +1,82 @@
+import Foundation
+import WebRTC
+
+protocol SignalClientDelegate: AnyObject {
+    func signalClientDidConnect(_ signalClient: SignalingClient)
+    func signalClientDidDisconnect(_ signalClient: SignalingClient)
+    func signalClient(_ signalClient: SignalingClient, didReceiveRemoteSdp sdp: RTCSessionDescription)
+    func signalClient(_ signalClient: SignalingClient, didReceiveCandidate candidate: RTCIceCandidate)
+}
+
+final class SignalingClient {
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    private let webSocket: WebSocketClient
+    weak var delegate: SignalClientDelegate?
+    
+    init(webSocket: WebSocketClient) {
+        self.webSocket = webSocket
+    }
+    
+    func connect() {
+        self.webSocket.delegate = self
+        self.webSocket.connect()
+    }
+    
+    func send(sdp rtcSdp: RTCSessionDescription) {
+        let message = SignalingMessage.sdp(SessionDescription(from: rtcSdp))
+        do {
+            let dataMessage = try self.encoder.encode(message)
+            
+            self.webSocket.send(data: dataMessage)
+        }
+        catch {
+            debugPrint("Warning: Could not encode sdp: \(error)")
+        }
+    }
+    
+    func send(candidate rtcIceCandidate: RTCIceCandidate) {
+        let message = SignalingMessage.candidate(IceCandidate(from: rtcIceCandidate))
+        do {
+            let dataMessage = try self.encoder.encode(message)
+            self.webSocket.send(data: dataMessage)
+        }
+        catch {
+            debugPrint("Warning: Could not encode candidate: \(error)")
+        }
+    }
+}
+
+extension SignalingClient: WebSocketClientDelegate {
+    func webSocketDidConnect(_ webSocket: WebSocketClient) {
+        self.delegate?.signalClientDidConnect(self)
+    }
+    
+    func webSocketDidDisconnect(_ webSocket: WebSocketClient) {
+        self.delegate?.signalClientDidDisconnect(self)
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            debugPrint("Trying to reconnect to signaling server...")
+            self.webSocket.connect()
+        }
+    }
+    
+    func webSocket(_ webSocket: WebSocketClient, didReceiveData data: Data) {
+        let message: SignalingMessage
+        
+        do {
+            message = try self.decoder.decode(SignalingMessage.self, from: data)
+        }
+        catch {
+            debugPrint("Warning: Could not decode incoming message: \(error)")
+            return
+        }
+        
+        switch message {
+        case .candidate(let iceCandidate):
+            self.delegate?.signalClient(self, didReceiveCandidate: iceCandidate.rtcIceCandidate)
+        case .sdp(let sessionDescription):
+            self.delegate?.signalClient(self, didReceiveRemoteSdp: sessionDescription.rtcSessionDescription)
+        }
+    }
+}
