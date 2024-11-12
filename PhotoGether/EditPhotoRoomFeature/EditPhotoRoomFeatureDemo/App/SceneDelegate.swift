b9a3c9ca1620c2213cd6297c0c7d7ca619b66eee
0Hooni <thddudgns972@gmail.com>
2024-11-12 16:07:09 +0900

    feat/#17 :: 호스트 편집화면 레이아웃 작업
    - BottomView 추가

--- PhotoGether/EditPhotoRoomFeature/EditPhotoRoomFeatureDemo/App/SceneDelegate.swift
+++ PhotoGether/EditPhotoRoomFeature/EditPhotoRoomFeatureDemo/App/SceneDelegate.swift
@@ -11,7 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = EditPhotoRoomViewController()
+        window?.rootViewController = EditPhotoRoomHostViewController()
         window?.makeKeyAndVisible()
     }
 }
