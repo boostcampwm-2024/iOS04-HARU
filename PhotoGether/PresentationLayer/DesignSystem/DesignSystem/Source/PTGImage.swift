0a04098957dc8b3a575b506ac2d4e1f2160a47e8
youn9k <ace_lephant@naver.com>
2024-11-12 17:59:54 +0900

    chore :: 아이콘 부활

--- PhotoGether/PresentationLayer/DesignSystem/DesignSystem/Source/PTGImage.swift
+++ PhotoGether/PresentationLayer/DesignSystem/DesignSystem/Source/PTGImage.swift
@@ -3,6 +3,8 @@ import UIKit
 public enum PTGImage {
     case frameIcon
     case stickerIcon
+    case chevronLeftWhite
+    case chevronRightBlack
     
     public var image: UIImage {
         switch self {
@@ -10,6 +12,10 @@ public enum PTGImage {
             return UIImage(resource: .frameIcon)
         case .stickerIcon:
             return UIImage(resource: .stickerIcon)
+        case .chevronLeftWhite:
+            return UIImage(resource: .chevronLeftWhite)
+        case .chevronRightBlack:
+            return UIImage(resource: .chevronRightBlack)
         }
     }
 }
