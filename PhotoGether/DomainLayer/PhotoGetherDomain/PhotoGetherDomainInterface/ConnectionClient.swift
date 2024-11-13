e86947658abd4c0692b1bb3ef34fb6ce71e3cd14
youn9k <ace_lephant@naver.com>
2024-11-13 15:41:16 +0900

    chore/#24 :: connect 접근제어자 수정

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainInterface/ConnectionClient.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainInterface/ConnectionClient.swift
@@ -4,7 +4,6 @@ import WebRTC
 public protocol ConnectionClient: SignalingClientDelegate, WebRTCClientDelegate {
     var remoteVideoView: UIView { get }
     
-    func connect()
     func sendOffer(offer: RTCSessionDescription)
     func sendData(data: Data)
 }
