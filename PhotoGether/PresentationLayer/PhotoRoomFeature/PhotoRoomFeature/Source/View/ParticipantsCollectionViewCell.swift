dae76a73c3cbaaf3de960bbc22190db365b5c2df
youn9k <ace_lephant@naver.com>
2024-11-14 16:41:46 +0900

    feat/#1 :: 참여자뷰Cell 구현
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>

---
+++ PhotoGether/PresentationLayer/PhotoRoomFeature/PhotoRoomFeature/Source/View/ParticipantsCollectionViewCell.swift
@@ -0,0 +1,65 @@
+import UIKit
+import BaseFeature
+import SnapKit
+
+public final class ParticipantsCollectionViewCell: UICollectionViewCell {
+    public static let reuseIdentifier = "\(ParticipantsCollectionViewCell.self)"
+    
+    private let nicknameLabel = UILabel()
+    private var videoView = UIView()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+        setConstraints()
+        configureUI()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    public func setNickname(_ nickname: String) {
+        self.nicknameLabel.text = nickname
+    }
+    
+    public func setVideoView(_ videoView: UIView) {
+        self.videoView = videoView
+    }
+    
+    private func addViews() {
+        [videoView, nicknameLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setConstraints() {
+        videoView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        nicknameLabel.snp.makeConstraints {
+            $0.width.equalTo(Constants.nicknameLabelWidth)
+            $0.height.equalTo(Constants.nicknameLabelHeight)
+            $0.top.equalToSuperview().offset(Constants.nicknameLabelTopSpacing)
+            $0.trailing.equalToSuperview().inset(Constants.nicknameLabelTrailingSpacing)
+        }
+    }
+    
+    private func configureUI() {
+        nicknameLabel.font = .systemFont(ofSize: 11)
+        nicknameLabel.setKern()
+        nicknameLabel.textColor = .white.withAlphaComponent(0.8)
+        nicknameLabel.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        nicknameLabel.layer.cornerRadius = 20
+    }
+}
+
+extension ParticipantsCollectionViewCell {
+    enum Constants {
+        static let nicknameLabelWidth: CGFloat = 40
+        static let nicknameLabelHeight: CGFloat = 20
+        static let nicknameLabelTopSpacing: CGFloat = 8
+        static let nicknameLabelTrailingSpacing: CGFloat = 8
+    }
+}
