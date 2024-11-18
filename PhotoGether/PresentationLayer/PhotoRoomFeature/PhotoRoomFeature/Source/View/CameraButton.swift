f7378cacdf4694867360354a923e61a7842f30ff
Kiyoung <rlaky45@naver.com>
2024-11-18 14:58:00 +0900

    feat/#44 :: 카메라 버튼 게스트 버전 구현
    - 기존 카메라 버튼에 Bool 값을 추가해 호스트와 게스트에 따라 모양이 바뀌도록 했습니다

--- PhotoGether/PresentationLayer/PhotoRoomFeature/PhotoRoomFeature/Source/View/CameraButton.swift
+++ PhotoGether/PresentationLayer/PhotoRoomFeature/PhotoRoomFeature/Source/View/CameraButton.swift
@@ -3,14 +3,19 @@ import DesignSystem
 
 final class CameraButton: UIButton {
     private let innerCircle = UIView()
+    private let innerEllipsis = UIImageView()
+    private let isHost: Bool
     
     // MARK: init
-    override init(frame: CGRect) {
+    init(isHost: Bool) {
+        self.isHost = isHost
         super.init(frame: .zero)
         
         addViews()
         setupContstraints()
         configureUI()
+        
+        guard isHost else { return }
         setActions()
     }
     
@@ -19,12 +24,18 @@ final class CameraButton: UIButton {
     }
     
     private func addViews() {
-        addSubview(innerCircle)
+        [innerCircle, innerEllipsis].forEach { addSubview($0) }
     }
     
     private func setupContstraints() {
         innerCircle.snp.makeConstraints {
             $0.width.height.equalTo(Constants.innerCircleSize)
+            $0.center.equalToSuperview()
+        }
+        
+        innerEllipsis.snp.makeConstraints {
+            $0.width.equalTo(Constants.ellipsisWidth)
+            $0.height.equalTo(Constants.ellipsisHeight)
             $0.center.equalToSuperview()
         }
     }
@@ -35,20 +46,23 @@ final class CameraButton: UIButton {
         self.layer.borderWidth = Constants.ringWidth
         
         innerCircle.isUserInteractionEnabled = false
-        innerCircle.backgroundColor = .white
+        innerCircle.backgroundColor = isHost ? .white : PTGColor.gray85.color
+        
+        innerEllipsis.isHidden = isHost
+        innerEllipsis.image = PTGImage.ellipsisIcon.image
     }
     
     // MARK: 터치 시 카메라 버튼 색상 변경
     private func setActions() {
         self.addAction(UIAction { [weak self] _ in
             guard let self else { return }
-            self.innerCircle.backgroundColor = PTGColor.gray30.color
+            self.innerCircle.backgroundColor = PTGColor.gray85.color
         }, for: .touchDown)
         
         self.addAction(UIAction { [weak self] _ in
             guard let self else { return }
             self.innerCircle.backgroundColor = .white
-        }, for: [.touchCancel, .touchUpOutside, .touchUpInside])
+        }, for: [.touchCancel, .touchUpOutside])
     }
     
     override func layoutSubviews() {
@@ -63,6 +77,9 @@ extension CameraButton {
     enum Constants {
         fileprivate static let ringWidth: CGFloat = 4
         fileprivate static let innerCircleSize: CGFloat = 52
+        fileprivate static let ellipsisWidth: CGFloat = 34
+        fileprivate static let ellipsisHeight: CGFloat = 6
+        
         static let buttonSize: CGFloat = 64
     }
 }
