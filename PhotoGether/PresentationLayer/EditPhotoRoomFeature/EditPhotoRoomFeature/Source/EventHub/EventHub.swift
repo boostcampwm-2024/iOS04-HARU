5c55f310a20f00d704e0442635c48aa6ce90d98f
0Hooni <thddudgns972@gmail.com>
2024-11-18 18:07:00 +0900

    feat/#47 :: updateEvent 메서드 구현
    Co-Authored-By: seuhong <66902876+hsw1920@users.noreply.github.com>

--- PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EventHub/EventHub.swift
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EventHub/EventHub.swift
@@ -23,7 +23,7 @@ final class EventManager {
         switch event.type {
         case .create: createEvent(by: event)
         case .delete: deleteEvent(by: event)
-        case .update: break
+        case .update: updateEvent(to: event)
         }
     }
     
@@ -74,6 +74,39 @@ final class EventManager {
         else {
             stickerDictionary[event.entity.id] = nil
             isObejctDeleted[event.entity.id] = true
+        }
+    }
+    
+    private func updateEvent(to event: EventEntity) {
+        guard isObejctDeleted[event.entity.id] == false else {
+            // TODO: 실패한 경우를 대비한 return이 필요
+            return
+        }
+        guard let oldSticker = stickerDictionary[event.entity.id] else {
+            // TODO: 실패한 경우를 대비한 return이 필요
+            return
+        }
+        
+        // MARK: 이건 말도 안됨
+        guard let newOwner = event.entity.owner else { return }
+        
+        // MARK: Old, NewOwner가 nil이 아닐 때
+        if let oldOwner = oldSticker.owner {
+            if oldOwner == newOwner {
+                // MARK: Old,New Owner가 서로 같을 때
+                stickerDictionary[event.entity.id] = event.entity
+                
+                return
+            } else {
+                // MARK: Old,New Owner가 서로 다를 때
+                // TODO: 그냥 적용 못할 때 -> oldSticker를 전파 해줘야된다.
+                
+                return
+            }
+        }
+        // MARK: OldOwner가 nil일 때
+        else {
+            stickerDictionary[event.entity.id] = event.entity
         }
     }
 }
