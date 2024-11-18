c9d79b29fdeba8a5e291475b52f1824e3f0d90bb
hsw1920 <hsw1920@gmail.com>
2024-11-18 18:00:06 +0900

    feat/#47 :: deleteEvent 메서드 구현
    Co-Authored-By: YeongHoon Song <37678646+0Hooni@users.noreply.github.com>

--- PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EventHub/EventHub.swift
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EventHub/EventHub.swift
@@ -14,7 +14,6 @@ final class EventHub {
     }
 }
 
-
 final class EventManager {
     var currentEvent: EventEntity? = nil
     var isObejctDeleted: [UUID: Bool] = [:]
@@ -23,7 +22,7 @@ final class EventManager {
     func work(event: EventEntity) {
         switch event.type {
         case .create: createEvent(by: event)
-        case .delete: break
+        case .delete: deleteEvent(by: event)
         case .update: break
         }
     }
@@ -40,5 +39,41 @@ final class EventManager {
         
         stickerDictionary[event.entity.id] = event.entity
         isObejctDeleted[event.entity.id] = false
+    }
+    
+    private func deleteEvent(by event: EventEntity) {
+        guard isObejctDeleted[event.entity.id] == false else {
+            // TODO: 실패한 경우를 대비한 return이 필요
+            return
+        }
+        
+        guard let oldSticker = stickerDictionary[event.entity.id] else {
+            // TODO: 실패한 경우를 대비한 return이 필요
+            return
+        }
+        
+        // MARK: 이건 말도 안됨
+        guard let newOwner = event.entity.owner else { return }
+        
+        // MARK: Old, NewOwner가 nil이 아닐 때
+        if let oldOwner = oldSticker.owner {
+            if oldOwner == newOwner {
+                // MARK: Old,New Owner가 서로 같을 때
+                stickerDictionary[event.entity.id] = nil
+                isObejctDeleted[event.entity.id] = true
+                
+                return
+            } else {
+                // MARK: Old,New Owner가 서로 다를 때
+                // TODO: 그냥 적용 못할 때 -> oldSticker를 전파 해줘야된다.
+                
+                return
+            }
+        }
+        // MARK: OldOwner가 nil일 때
+        else {
+            stickerDictionary[event.entity.id] = nil
+            isObejctDeleted[event.entity.id] = true
+        }
     }
 }
