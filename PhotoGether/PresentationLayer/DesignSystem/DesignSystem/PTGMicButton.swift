98575b57fdcad7175af385f3284523782d6efe81
youn9k <ace_lephant@naver.com>
2024-11-19 09:43:03 +0900

    feat/#12 :: 커스텀 마이크 버튼 구현
    Co-Authored-By: YeongHoon Song <37678646+0Hooni@users.noreply.github.com>

---
+++ PhotoGether/PresentationLayer/DesignSystem/DesignSystem/PTGMicButton.swift
@@ -0,0 +1,81 @@
+import UIKit
+import SnapKit
+
+public final class PTGMicButton: UIButton {
+    private let buttonImage = UIImageView()
+    private var micState: PTGMicState
+    
+    public init(micState: PTGMicState) {
+        self.micState = micState
+        super.init(frame: .zero)
+        
+        addViews()
+        setupConstraints()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        addSubview(buttonImage)
+    }
+    
+    private func setupConstraints() {
+        buttonImage.snp.makeConstraints {
+            $0.width.height.equalTo(24)
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    private func configureUI() {
+        backgroundColor = .white.withAlphaComponent(0.2)
+        
+        buttonImage.contentMode = .scaleAspectFit
+        buttonImage.image = UIImage(systemName: micState.image)
+        buttonImage.tintColor = micState.color
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.width / 2
+    }
+    
+    public func toggleMicState() {
+        switch micState {
+        case .on:
+            micState = .off
+        case .off:
+            micState = .on
+        }
+        
+        buttonImage.image = UIImage(systemName: micState.image)
+        buttonImage.tintColor = micState.color
+    }
+}
+
+public extension PTGMicButton {
+    enum PTGMicState {
+        case on
+        case off
+        
+        var image: String {
+            switch self {
+            case .on: 
+                return "microphone"
+            case .off: 
+                return "microphone.slash"
+            }
+        }
+        
+        var color: UIColor {
+            switch self {
+            case .on:
+                return .white
+            case .off:
+                return .red
+            }
+        }
+    }
+}
