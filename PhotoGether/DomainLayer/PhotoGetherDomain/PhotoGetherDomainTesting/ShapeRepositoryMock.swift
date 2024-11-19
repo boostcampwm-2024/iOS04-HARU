20bfcdcbdbe87ad4395dc789795cee2c7889c364
0Hooni <thddudgns972@gmail.com>
2024-11-19 22:46:54 +0900

    fix/#53 :: 더미 이미지 데이터화 문제 해결
    - 기존의 dataRepresentation은 URL 자체를 데이터화 하는 방식
    - 이미지 파일 자체를 데이터화 하기 위해서는 Data(contentOf: ) 형태의 생성자를 사용해야 됐음

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainTesting/ShapeRepositoryMock.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainTesting/ShapeRepositoryMock.swift
@@ -11,14 +11,17 @@ public final class ShapeRepositoryMock: ShapeRepository {
     
     public func fetchStickerList() -> AnyPublisher<[Data], Never> {
         let imageDataList = imageNameList.map { imageData(named: $0) }
+        
         return Just(imageDataList.compactMap { $0 })
             .eraseToAnyPublisher()
     }
     
     private func imageData(named: String) -> Data? {
-        let bundle = Bundle(for: Self.self)
-        let imageData = bundle.url(forResource: named, withExtension: "svg")?
-            .dataRepresentation
+        let bundle = Bundle(for: Self.self) // 해당 클래스가 존재하는 Bundle을 의미
+        guard let imageURL = bundle.url(forResource: named, withExtension: "png"),
+              let imageData = try? Data(contentsOf: imageURL)
+        else { debugPrint("bundle Image to Data error"); return nil }
+        
         return imageData
     }
 }
