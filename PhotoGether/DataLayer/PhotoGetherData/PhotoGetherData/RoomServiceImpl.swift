bbace5a366298b1bb76f28b21d092f4fd8db51b3
Kiyoung <rlaky45@naver.com>
2024-11-20 21:49:30 +0900

    feat/#40 :: RoomService 구현
    - RoomService interface와 Impl 구현
    
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>

---
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/RoomServiceImpl.swift
@@ -0,0 +1,32 @@
+import Foundation
+import PhotoGetherNetwork
+
+public final class RoomServiceImpl: RoomService {
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    private var webSocketClient: WebSocketClient
+    
+    init(webSocketClient: WebSocketClient) {
+        self.webSocketClient = webSocketClient
+    }
+    
+    public func send(request: WebSocketRequest) {
+        guard let data = request.toData(encoder: encoder) else {
+            debugPrint("방 생성 요청 데이터 인코딩 실패: \(request)")
+            return
+        }
+        
+        webSocketClient.send(data: data)
+    }
+}
+
+// MARK: WebSocketDelegate
+extension RoomServiceImpl: WebSocketClientDelegate {
+    public func webSocketDidConnect(_ webSocket: WebSocketClient) { }
+    
+    public func webSocketDidDisconnect(_ webSocket: WebSocketClient) { }
+    
+    public func webSocket(_ webSocket: WebSocketClient, didReceiveData data: Data) {
+        // TODO: 생성된 방번호 고유 아이디가 담긴 정보 디코딩
+    }
+}
