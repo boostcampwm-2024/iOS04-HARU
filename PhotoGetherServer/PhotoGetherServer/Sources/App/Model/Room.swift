f30ed1e2c0cee6e2f2f7994a23e3fb504493f235
youn9k <ace_lephant@naver.com>
2024-11-20 19:58:03 +0900

    feat/#40 :: 방 생성, 초대, 추방 메소드 추가
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>

--- PhotoGetherServer/PhotoGetherServer/Sources/App/Model/Room.swift
+++ PhotoGetherServer/PhotoGetherServer/Sources/App/Model/Room.swift
@@ -1,4 +1,25 @@
 struct Room {
     private var users: [User] = []
+    private var maxCount: Int = 4
     let roomID: String
+
+    
+    init(roomID: String) {
+        self.roomID = roomID
+    }
+    
+    @discardableResult
+    mutating func invite(user: User) -> Bool {
+        guard users.count < maxCount else { return false }
+        users.append(user)
+        return true
+    }
+    
+    @discardableResult
+    mutating func kick(userID: String) -> Bool {
+        let filtered = users.filter { $0.id != userID }
+        users = filtered
+        return filtered.isEmpty // 필터에 걸렸으면 찾은 것
+    }
+    
 }
