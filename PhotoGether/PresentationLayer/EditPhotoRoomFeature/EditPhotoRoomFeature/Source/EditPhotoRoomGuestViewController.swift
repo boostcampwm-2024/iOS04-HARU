871f1de3dda23e969f6362ba84cc0ea069bfbecf
seuhong <66902876+hsw1920@users.noreply.github.com>
2024-11-21 11:33:30 +0900

    fix/#59 :: 명시적 스토리보드 제한
    Co-authored-by: Youngkyu Song <ace_lephant@naver.com>

--- PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EditPhotoRoomGuestViewController.swift
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EditPhotoRoomGuestViewController.swift
@@ -18,6 +18,7 @@ public class EditPhotoRoomGuestViewController: BaseViewController, ViewControlle
         super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
