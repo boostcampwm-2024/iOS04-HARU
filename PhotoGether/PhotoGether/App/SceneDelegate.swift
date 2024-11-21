3f0554a20a4869f9e02ae31f5d9a611f8c48122d
youn9k <ace_lephant@naver.com>
2024-11-21 23:03:50 +0900

    feat/#70 :: App 모듈에서 빌드할 수 있도록 수정
    SceneDelegate 연결, 모듈 의존성 연결

--- PhotoGether/PhotoGether/App/SceneDelegate.swift
+++ PhotoGether/PhotoGether/App/SceneDelegate.swift
@@ -1,6 +1,12 @@
 import UIKit
 import PhotoGetherNetwork
 import PhotoGetherData
+import PhotoGetherDomainInterface
+import PhotoGetherDomain
+import WaitingRoomFeature
+import PhotoRoomFeature
+import EditPhotoRoomFeature
+import SharePhotoFeature
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -11,15 +17,78 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let urlString = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String ?? ""
+        let url = URL(string: urlString)!
+        debugPrint("SignalingServer URL: \(url)")
+        
+        let webScoketClient: WebSocketClient = WebSocketClientImpl(url: url)
+        
+        let roomService: RoomService = RoomServiceImpl(
+            webSocketClient: webScoketClient
+        )
+        
+        let signalingService: SignalingService = SignalingServiceImpl(
+            webSocketClient: webScoketClient
+        )
+        
+        let webRTCService: WebRTCService = WebRTCServiceImpl(
+            iceServers: [
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302"
+            ]
+        )
+        
+        let connectionClient: ConnectionClient = ConnectionClientImpl(
+            signalingService: signalingService,
+            webRTCService: webRTCService
+        )
+
+        let connectionRepository: ConnectionRepository = ConnectionRepositoryImpl(
+            clients: [connectionClient]
+        )
+        
+        let sendOfferUseCase: SendOfferUseCase = SendOfferUseCaseImpl(
+            repository: connectionRepository
+        )
+        
+        let getLocalVideoUseCase: GetLocalVideoUseCase = GetLocalVideoUseCaseImpl(
+            connectionRepository: connectionRepository
+        )
+        
+        let getRemoteVideoUseCase: GetRemoteVideoUseCase = GetRemoteVideoUseCaseImpl(
+            connectionRepository: connectionRepository
+        )
+        
+        let captureVideosUseCase: CaptureVideosUseCase = CaptureVideosUseCaseImpl(
+            connectionRepository: connectionRepository
+        )
+        
+        let photoRoomViewModel: PhotoRoomViewModel = PhotoRoomViewModel(
+            captureVideosUseCase: captureVideosUseCase
+        )
+        
+        let photoRoomViewController: PhotoRoomViewController = PhotoRoomViewController(
+            connectionRepsitory: connectionRepository,
+            viewModel: photoRoomViewModel,
+            isHost: true
+        )
+        
+        let viewModel: WaitingRoomViewModel = WaitingRoomViewModel(
+            sendOfferUseCase: sendOfferUseCase,
+            getLocalVideoUseCase: getLocalVideoUseCase,
+            getRemoteVideoUseCase: getRemoteVideoUseCase
+        )
+        
+        let viewController: WaitingRoomViewController = WaitingRoomViewController(
+            viewModel: viewModel,
+            photoRoomViewController: photoRoomViewController
+        )
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: viewController)
         window?.makeKeyAndVisible()
-        
-        let baseURLString = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String ?? ""
-        
-        let webSocketClient: WebSocketClient = WebSocketClientImpl(url: URL(string: baseURLString)!)
-        let signalingService: SignalingService = SignalingServiceImpl(webSocketClient: webSocketClient)
-        
-        signalingService.connect()
     }
 }
