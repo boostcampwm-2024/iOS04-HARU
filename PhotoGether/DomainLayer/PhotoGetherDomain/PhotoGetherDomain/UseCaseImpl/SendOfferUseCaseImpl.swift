91ec65eb4594c0cac152e391c21dbfe3fe0f8ace
youn9k <ace_lephant@naver.com>
2024-11-21 15:16:46 +0900

    feat/#40 :: SendOfferUseCase 구현
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>

---
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/SendOfferUseCaseImpl.swift
@@ -0,0 +1,16 @@
+import Foundation
+import PhotoGetherDomainInterface
+
+public final class SendOfferUseCaseImpl: SendOfferUseCase {
+    public func execute() {
+        // TODO: 특정 Peer에게만 Offer를 보내도록 수정해야 함
+        guard let client = repository.clients.first else { return }
+        client.sendOffer()
+    }
+    
+    private let repository: ConnectionRepository
+    
+    init(repository: ConnectionRepository) {
+        self.repository = repository
+    }
+}
