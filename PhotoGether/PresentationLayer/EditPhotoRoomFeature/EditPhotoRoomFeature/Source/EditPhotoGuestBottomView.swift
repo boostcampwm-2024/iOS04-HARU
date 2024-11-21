20a8d337f64b92a44416540c5c61709a06362ae9
hsw1920 <hsw1920@gmail.com>
2024-11-21 11:37:02 +0900

    style/#59 :: 불필요한 공백 제거

--- PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EditPhotoGuestBottomView.swift
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EditPhotoGuestBottomView.swift
@@ -27,7 +27,6 @@ final class EditPhotoGuestBottomView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
     
     private func addViews() {
         addSubview(stackView)
