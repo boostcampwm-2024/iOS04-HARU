95887bb57eb6665bd3a7ed6fa28f0bb94c3b69bc
hsw1920 <hsw1920@gmail.com>
2024-11-25 00:41:47 +0900

    feat/#77 StickerView UI 구현

---
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/View/StickerView.swift
@@ -0,0 +1,61 @@
+import DesignSystem
+import PhotoGetherDomainInterface
+import UIKit
+
+final class StickerView: UIImageView {
+    private let nicknameLabel = UILabel()
+
+    private var sticker: StickerEntity
+
+
+    init(sticker: StickerEntity) {
+        self.sticker = sticker
+        super.init(frame: sticker.frame)
+        setupTapGesture()
+        addViews()
+        setupConstraints()
+        configureUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        addSubview(nicknameLabel)
+    }
+    
+    private func setupConstraints() {
+        nicknameLabel.snp.makeConstraints {
+            $0.top.equalTo(snp.bottom)
+            $0.trailing.equalTo(snp.trailing)
+        }
+    }
+    
+    private func configureUI() {
+        layer.borderColor = PTGColor.primaryGreen.color.cgColor
+        setImage(to: sticker.image)
+        
+        sticker.owner != nil
+        ? (layer.borderWidth = 1)
+        : (layer.borderWidth = 0)
+    }
+
+    private func setupTapGesture() {
+        isUserInteractionEnabled = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        addGestureRecognizer(tapGesture)
+    }
+    
+    private func setImage(to urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        Task { [weak self] in
+            guard let (data, _) = try? await URLSession.shared.data(from: url)
+            else { return }
+            
+            self?.image = UIImage(data: data)
+        }
+    }
