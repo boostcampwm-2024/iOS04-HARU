1bbb761e20681e7592a30f675014679dfd27b483
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2024-11-25 15:47:43 +0900

    fix/#65 :: Repository 수정에 따라 UseCase 수정
    - Repository에 LocalVideoView를 생성하면서 연관된 UseCase들을 수정했습니다.
    
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/GetLocalVideoUseCaseImpl.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/GetLocalVideoUseCaseImpl.swift
@@ -5,7 +5,7 @@ import PhotoGetherDomainInterface
 public final class GetLocalVideoUseCaseImpl: GetLocalVideoUseCase {
     public func execute() -> UIView {
         // TODO: 리포지토리에서 하나의 localVideoView만 들고 있도록 변경 예정
-        connectionRepository.clients[0].localVideoView
+        connectionRepository.localVideoView
     }
     
     private let connectionRepository: ConnectionRepository
