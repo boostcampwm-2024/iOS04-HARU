4bca494e2d3bc488432dfa55b7c27f687a472b97
0Hooni <thddudgns972@gmail.com>
2024-11-25 23:12:33 +0900

    feat/#103 :: 캐시 매니저를 사용하여 캐싱 구현

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/LocalShapeDataSourceImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/LocalShapeDataSourceImpl.swift
@@ -1,10 +1,16 @@
 import Combine
 import Foundation
 import PhotoGetherDomainInterface
+import PhotoGetherNetwork
 
 public final class LocalShapeDataSourceImpl: ShapeDataSource {
-        return Empty().eraseToAnyPublisher()
     public func fetchEmojiData(_ endpoint: EndPoint) -> AnyPublisher<[EmojiDTO], Error> {
+        guard let url = endpoint.request().url else { return Empty().eraseToAnyPublisher() }
+        
+        return CacheManager().loadPublisher(key: url.absoluteString)
+            .compactMap { $0 }
+            .decode(type: [EmojiDTO].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
     }
     
     public init() { }
--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/RemoteShapeDataSourceImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/RemoteShapeDataSourceImpl.swift
@@ -6,6 +6,15 @@ public final class RemoteShapeDataSourceImpl: ShapeDataSource {
     // TODO: 페이징 적용 필요
     public func fetchEmojiData(_ endpoint: EndPoint) -> AnyPublisher<[EmojiDTO], Error> {
         return Request.requestJSON(endpoint)
+            .map { (emojiDTOs: [EmojiDTO]) -> [EmojiDTO] in
+                if let data = try? JSONEncoder().encode(emojiDTOs),
+                   let url = endpoint.request().url {
+                    CacheManager().save(key: url.absoluteString, data: data)
+                }
+                
+                return emojiDTOs
+            }
+            .eraseToAnyPublisher()
     }
     
     public init() { }
