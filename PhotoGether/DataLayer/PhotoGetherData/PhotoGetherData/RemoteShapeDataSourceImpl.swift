efcb27297a4ead8784c2e31b245dfb0d156fab20
0Hooni <thddudgns972@gmail.com>
2024-11-25 23:07:12 +0900

    refactor/#103 :: EndPoint 위치 변경
    - 캐시 Path와 Request Path를 동일하게 사용하기 위해 위치 변경

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/RemoteShapeDataSourceImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/RemoteShapeDataSourceImpl.swift
@@ -10,16 +10,3 @@ public final class RemoteShapeDataSourceImpl: ShapeDataSource {
     
     public init() { }
 }
-
-private struct EmojiEndPoint: EndPoint {
-    var baseURL: URL { URL(string: "https://api.api-ninjas.com")! }
-    var path: String { "v1/emoji" }
-    var method: HTTPMethod { .get }
-    // 시작 인덱스, 1회 호출당 30개씩 호출
-    var parameters: [String: Any]? { ["group": "objects", "offset": 0] }
-    var headers: [String: String]? {
-        let apiKey = Bundle.main.object(forInfoDictionaryKey: "EMOJI_API_KEY") as? String ?? ""
-        return ["X-Api-Key": apiKey]
-    }
-    var body: Encodable? { nil }
-}
--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ShapeRepositoryImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ShapeRepositoryImpl.swift
@@ -23,3 +23,16 @@ final public class ShapeRepositoryImpl: ShapeRepository {
         self.remoteDataSource = remoteDataSource
     }
 }
+
+fileprivate struct EmojiEndPoint: EndPoint {
+    var baseURL: URL { URL(string: "https://api.api-ninjas.com")! }
+    var path: String { "v1/emoji" }
+    var method: HTTPMethod { .get }
+    // 시작 인덱스, 1회 호출당 30개씩 호출
+    var parameters: [String: Any]? { ["group": "objects", "offset": 0] }
+    var headers: [String: String]? {
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "EMOJI_API_KEY") as? String ?? ""
+        return ["X-Api-Key": apiKey]
+    }
+    var body: Encodable? { nil }
+}
