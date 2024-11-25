aa38db9ec1f73bc9d9979dcaeb21c4f3223d7f76
youn9k <ace_lephant@naver.com>
2024-11-25 14:39:08 +0900

    feat/#95 :: LocalVideoView 단일화
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ConnectionRepositoryImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ConnectionRepositoryImpl.swift
@@ -4,7 +4,17 @@ import PhotoGetherDomainInterface
 public final class ConnectionRepositoryImpl: ConnectionRepository {
     public var clients: [ConnectionClient]
     
-    public init(clients: [ConnectionClient]) {
+    private let localVideoView = CapturableVideoView()
+    
+    private let roomService: RoomService
+    
+    public init(clients: [ConnectionClient], roomService: RoomService) {
         self.clients = clients
+        self.roomService = roomService
+        bindLocalVideo()
+    }
+    
+    private func bindLocalVideo() {
+        self.clients.forEach { $0.bindLocalVideo(localVideoView) }
     }
 }
--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainInterface/ConnectionClient.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainInterface/ConnectionClient.swift
@@ -4,11 +4,11 @@ import Combine
 
 public protocol ConnectionClient {
     var remoteVideoView: UIView { get }
-    var localVideoView: UIView { get }
     
     var receivedDataPublisher: PassthroughSubject<Data, Never> { get }
     
     func sendOffer()
     func sendData(data: Data)
-    func captureVideos() -> [UIImage]
+    func captureVideo() -> UIImage
+    func bindLocalVideo(_ localVideoView: UIView)
 }
