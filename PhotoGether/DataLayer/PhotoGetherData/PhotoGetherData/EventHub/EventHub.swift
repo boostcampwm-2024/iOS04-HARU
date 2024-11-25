bd01422c276b85cfd2ede9b2dfbcbebff825d818
hsw1920 <hsw1920@gmail.com>
2024-11-25 17:36:12 +0900

    refactor/#101 :: EventManager의 책임 분리
    - 현재 EventHub의 EventManager는 오직 StickerEntity를 담은 Event만을 관리할 수 있습니다.
    - 하지만 EventHub는 StickerEntity 이외에도 FrameEntity를 담은 Event 또한 관리해야합니다.
    - 현재 EventManager에서는 관련 StickerEntity를 관리하는 딕셔너리가 존재하기 때문에 해당 Manager에서 Frame 까지 함께 관리한다면 SRP에 위배됩니다.
    - 따라서 현재의 EventManager를 StickerEntity를 담은 Event만을 담당하는 Manager 역할을 할 수 있도록 리팩터링 하였습니다.
    - 다음 커밋에서 네이밍 변경이 있습니다.

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/EventHub/EventHub.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/EventHub/EventHub.swift
@@ -43,7 +43,14 @@ final class EventHub {
                     debugPrint("popLast 에러")
                     return
                 }
-                self?.eventManager.work(event: currentEvent)
+                
+                switch currentEvent.payload {
+                case .sticker(let stickerEntity):
+                    self?.eventManager.work(type: currentEvent.type, with: stickerEntity)
+                case .frame(let frameEntity):
+                    // TODO: FrameManager
+                    break
+                }
             }
             .store(in: &cancellables)
     }
@@ -64,87 +71,88 @@ final class EventManager {
         return stickerDictionary.compactMap { $0.value }
     }
     
-    func work(event: EventEntity) {
+    func work(type: EventType, with sticker: StickerEntity) {
         callEventPublisher.send(false)
-        switch event.type {
-        case .create: createEvent(by: event)
-        case .delete: deleteEvent(by: event)
-        case .update: updateEvent(to: event)
-        case .unlock: unlockEvent(to: event)
+        switch type {
+        case .create: createEvent(by: sticker)
+        case .delete: deleteEvent(by: sticker)
+        case .update: updateEvent(by: sticker)
+        case .unlock: unlockEvent(by: sticker)
         }
         callEventPublisher.send(true)
     }
     
-    private func createEvent(by event: EventEntity) {
-        guard isObejctDeleted[event.entity.id] == nil else {
-            debugPrint("Create event 실패")
-            return
-        }
-        guard stickerDictionary[event.entity.id] == nil else {
+    private func createEvent(by sticker: StickerEntity) {
+        guard isObejctDeleted[sticker.id] == nil else {
             debugPrint("Create event 실패")
             return
         }
         
-        stickerDictionary[event.entity.id] = event.entity
-        isObejctDeleted[event.entity.id] = false
+        guard stickerDictionary[sticker.id] == nil else {
+            debugPrint("Create event 실패")
+            return
+        }
+        
+        stickerDictionary[sticker.id] = sticker
+        isObejctDeleted[sticker.id] = false
         
         // MARK: event가 아니라 전체 딕셔너리 보내기
         resultEventPublihser.send(currenntStickerList)
     }
     
-    private func deleteEvent(by event: EventEntity) {
-        guard isObejctDeleted[event.entity.id] == false,            // 이미 지워진 객체를 지우려 할 때
-              let oldSticker = stickerDictionary[event.entity.id]   // 해당 스티커가 없을 때(전파 받기 전에 지워짐)
+    private func deleteEvent(by sticker: StickerEntity) {
+        guard isObejctDeleted[sticker.id] == false,            // 이미 지워진 객체를 지우려 할 때
+              let oldSticker = stickerDictionary[sticker.id]   // 해당 스티커가 없을 때(전파 받기 전에 지워짐)
         else {
             // 이미 처리된 상황이기에 아무 처리를 하지 않아도 문제가 없음
             debugPrint("A/B 삭제 경쟁 상황에서 이미 처리된 삭제를 요청함")
             return
         }
         
-        guard let newOwner = event.entity.owner else { return }
+        guard let newOwner = sticker.owner else { return }
 
         if oldSticker.owner == nil || oldSticker.owner == newOwner {
             // OldOwner가 nil이거나 Old,New Owner가 서로 같을 때
-            stickerDictionary[event.entity.id] = nil
-            isObejctDeleted[event.entity.id] = true
+            stickerDictionary[sticker.id] = nil
+            isObejctDeleted[sticker.id] = true
             
             resultEventPublihser.send(currenntStickerList)
         }
     }
     
-    private func updateEvent(to event: EventEntity) {
-        guard isObejctDeleted[event.entity.id] == false,            // 이미 지워진 객체를 업데이트 하려 할 때
-              let oldSticker = stickerDictionary[event.entity.id]   // 해당 스티커가 없을 때(전파 받기 전에 지워짐)
+    private func updateEvent(by sticker: StickerEntity) {
+        guard isObejctDeleted[sticker.id] == false,            // 이미 지워진 객체를 업데이트 하려 할 때
+              let oldSticker = stickerDictionary[sticker.id]   // 해당 스티커가 없을 때(전파 받기 전에 지워짐)
         else {
             // 이미 처리된 상황이기에 아무 처리를 하지 않아도 문제가 없음
             debugPrint("A/B 경쟁 상황에서 이미 삭제된 객체의 업데이트를 요청함")
             return
         }
         
-        guard let newOwner = event.entity.owner else { return }
+        guard let newOwner = sticker.owner else { return }
 
         if oldSticker.owner == nil || oldSticker.owner == newOwner {
             // OldOwner가 nil이거나 Old,New Owner가 서로 같을 때
-            stickerDictionary[event.entity.id] = event.entity
+            stickerDictionary[sticker.id] = sticker
 
             resultEventPublihser.send(currenntStickerList)
         }
     }
     
-    private func unlockEvent(to event: EventEntity) {
-        guard isObejctDeleted[event.entity.id] == false,
-              let oldSticker = stickerDictionary[event.entity.id]
+    private func unlockEvent(by sticker: StickerEntity) {
+        guard isObejctDeleted[sticker.id] == false,
+              let oldSticker = stickerDictionary[sticker.id]
         else {
             // 이미 처리된 상황이기에 아무 처리를 하지 않아도 문제가 없음
             debugPrint("A/B 경쟁 상황에서 이미 삭제된 객체의 언락을 요청함")
             return
         }
         
-        if oldSticker.owner == event.entity.owner {
-            var newSticker = stickerDictionary[event.entity.id]
+        if oldSticker.owner == sticker.owner {
+            var newSticker = stickerDictionary[sticker.id]
             newSticker?.updateOwner(to: nil)
             
-            stickerDictionary[event.entity.id] = newSticker
+            stickerDictionary[sticker.id] = newSticker
             resultEventPublihser.send(currenntStickerList)
         }
     }
