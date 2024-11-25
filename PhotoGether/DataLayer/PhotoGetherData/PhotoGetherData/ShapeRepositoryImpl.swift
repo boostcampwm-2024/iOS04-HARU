0eaace15d25c47b6260378d13e20c07f6a644b09
0Hooni <thddudgns972@gmail.com>
2024-11-25 23:09:20 +0900

    chore/#103 :: 일부 주석 추가/제거

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ShapeRepositoryImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ShapeRepositoryImpl.swift
@@ -3,8 +3,6 @@ import Foundation
 import PhotoGetherDomainInterface
 
 final public class ShapeRepositoryImpl: ShapeRepository {
-    // TODO: local 먼저 확인 -> remote 데이터 확인하도록 수정
-    // MARK: JSON 데이터 내부의 URL을 어느 시점에 다운로드 할것인가...?
     public func fetchEmojiList() -> AnyPublisher<[EmojiEntity], Never> {
         return remoteDataSource.fetchEmojiData()
             .map { $0.map { $0.toEntity() } }
@@ -28,7 +26,7 @@ fileprivate struct EmojiEndPoint: EndPoint {
     var baseURL: URL { URL(string: "https://api.api-ninjas.com")! }
     var path: String { "v1/emoji" }
     var method: HTTPMethod { .get }
-    // 시작 인덱스, 1회 호출당 30개씩 호출
+    /// offset 기준 30개씩 호출
     var parameters: [String: Any]? { ["group": "objects", "offset": 0] }
     var headers: [String: String]? {
         let apiKey = Bundle.main.object(forInfoDictionaryKey: "EMOJI_API_KEY") as? String ?? ""
