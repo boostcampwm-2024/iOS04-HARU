88ce472bf7d6adf3ff547f1ed0d23aefbd35f627
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2024-11-25 13:58:24 +0900

    feat/#95 :: Server의 CreateRoomResponseDTO 수정
    - toData를 Encodable의 Extension으로 구현해서 기존 부분을 삭제했습니다
    
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>

--- PhotoGetherServer/PhotoGetherServer/Sources/App/DTO/CreateRoomResponseDTO.swift
+++ PhotoGetherServer/PhotoGetherServer/Sources/App/DTO/CreateRoomResponseDTO.swift
@@ -3,8 +3,5 @@ import Foundation
 package struct CreateRoomResponseDTO: Encodable {
     let roomID: String
     let userID: String
-    
-    func toData(_ encoder: JSONEncoder) -> Data? {
-        return try? encoder.encode(self)
-    }
 }
+
