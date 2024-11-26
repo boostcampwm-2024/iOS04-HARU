9d0f7dbeef37e317325268bb724da32634812778
0Hooni <thddudgns972@gmail.com>
2024-11-26 11:03:42 +0900

    fix/#72 :: PR 리뷰 반영

--- PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/StickerBottomSheetViewController.swift
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/StickerBottomSheetViewController.swift
@@ -27,7 +27,9 @@ public final class StickerBottomSheetViewController: UIViewController, ViewContr
     }
     
     @available(*, unavailable)
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
