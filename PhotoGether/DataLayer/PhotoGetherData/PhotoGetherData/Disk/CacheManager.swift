53450d2338748b8873070e513d00349297b34812
0Hooni <thddudgns972@gmail.com>
2024-11-26 19:09:31 +0900

    feat/#103 :: URL Hash 메서드 구현
    - 기본 hashValue는 매 실행시 다른 결과를 냄
    - SHA256 해싱 방식을 이용하도록 변경

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/Disk/CacheManager.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/Disk/CacheManager.swift
@@ -1,4 +1,5 @@
 import Combine
+import CryptoKit
 import OSLog
 import UIKit
 
@@ -17,8 +18,6 @@ final class CacheManager {
     ///
     /// key(URL String)와 data를 Disk cache에 저장
     /// key의 hash값을 path로 설정하여 중복 방지
-    func save(key: String, data: Data) {
-        let fileURL = cacheDirectory.appendingPathComponent(key.hashValue.description)
         
         do { try data.write(to: fileURL) }
         catch { os_log("ERROR: Failed to save data to disk\n\(error.localizedDescription)") }
@@ -31,13 +30,17 @@ final class CacheManager {
         let fileURL = cacheDirectory.appendingPathComponent(key.hashValue.description)
         
         return try? Data(contentsOf: fileURL)
+    func save(url: URL, data: Data) {
+        let key = hashKey(from: url)
+        let fileURL = cacheDirectory.appendingPathComponent(key)
     }
     
     /// 캐시 데이터 불러오기
     ///
     /// key(URL String)을 기반으로 캐시 메모리를 탐색하여 AnyPublisher<Data?, Error>를 반환
-    func loadPublisher(key: String) -> AnyPublisher<Data?, Error> {
-        let fileURL = cacheDirectory.appendingPathComponent(key.hashValue.description)
+    func loadPublisher(url: URL) -> AnyPublisher<Data?, Error> {
+        let key = hashKey(from: url)
+        let fileURL = cacheDirectory.appendingPathComponent(key)
         
         return Future<Data?, Error> { promise in
             do {
@@ -49,6 +52,12 @@ final class CacheManager {
             }
         }
         .eraseToAnyPublisher()
+    }
+    
+    private func hashKey(from url: URL) -> String {
+        let inputData = Data(url.absoluteString.utf8)
+        let hashed = SHA256.hash(data: inputData)
+        return hashed.compactMap { String(format: "%02x", $0) }.joined()
     }
 }
 
