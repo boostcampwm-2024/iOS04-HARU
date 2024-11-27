10bd74c59f1fec3f5e5100ecf9ae9d1a201dad3c
youn9k <ace_lephant@naver.com>
2024-11-27 11:17:43 +0900

    refactor/#112 :: 옵셔널 강제 언래핑 제거

--- PhotoGether/PresentationLayer/BaseFeature/BaseFeature/Extension/UIImageView+SetAsyncImage.swift
+++ PhotoGether/PresentationLayer/BaseFeature/BaseFeature/Extension/UIImageView+SetAsyncImage.swift
@@ -3,8 +3,13 @@ import UIKit
 public extension UIImageView {
     @MainActor
     func setAsyncImage(_ url: URL) async {
-        let cachedImage = ImageCache.readCache(with: url)
-        if cachedImage == nil { // MARK: 캐시 히트에 실패한 경우
+        if let cachedImage = ImageCache.readCache(with: url) { // MARK: 캐시 히트
+            guard let cachedUIImage = UIImage(data: cachedImage.imageData) else {
+                debugPrint("캐싱이미지 변환에 실패했습니다. \(url)")
+                return
+            }
+            self.image = cachedUIImage
+        } else { // MARK: 캐시 히트에 실패한 경우
             do {
                 let (data, response) = try await URLSession.shared.data(from: url)
                 guard
@@ -23,13 +28,6 @@ public extension UIImageView {
             } catch {
                 debugPrint("이미지 다운로드 중 오류 발생: \(error.localizedDescription)")
             }
-        } else { // MARK: 캐시 히트
-            guard let cachedUIImage = UIImage(data: cachedImage!.imageData) else {
-                debugPrint("캐싱이미지 변환에 실패했습니다. \(url)")
-                return
-            }
-            
-            self.image = cachedUIImage
         }
     }
 }
