425d4378a6b8b14f7b2be290c4e6629f3ea43585
youn9k <ace_lephant@naver.com>
2024-11-27 20:59:07 +0900

    feat/#110 :: JoinRoomUseCaseImpl 로직 추가
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/JoinRoomUseCaseImpl.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/JoinRoomUseCaseImpl.swift
@@ -3,13 +3,9 @@ import Combine
 import PhotoGetherDomainInterface
 
 public final class JoinRoomUseCaseImpl: JoinRoomUseCase {
-    // TODO: 수정 필요
     public func execute(roomID: String, hostID: String) -> AnyPublisher<Bool, Never> {
-        connectionRepository.joinRoom(to: roomID, hostID: hostID)
-            .self
-        
-        return Just(true)
-            .delay(for: .seconds(3), scheduler: DispatchQueue.main)
+        return connectionRepository.joinRoom(to: roomID, hostID: hostID)
+            .replaceError(with: false)
             .eraseToAnyPublisher()
     }
     
