a26af7b02e8abfd606d06543d98dee68b32b86e3
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2024-11-28 19:19:40 +0900

    feat/#128 :: 테스트용 서버 엔드포인트 추가
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>

--- PhotoGetherServer/PhotoGetherServer/Sources/App/routes.swift
+++ PhotoGetherServer/PhotoGetherServer/Sources/App/routes.swift
@@ -2,6 +2,7 @@ import Foundation
 import Vapor
 
 func routes(_ app: Application) throws {
+    var connectedClients = [WebSocket]()
     // MARK: 객체 초기화
     let roomManager = RoomManager()
     let webSocketController = WebSocketController(roomManager: roomManager)
@@ -9,5 +10,27 @@ func routes(_ app: Application) throws {
     // MARK: Controller에서 대신 처리
     app.webSocket("signaling") { req, client in
         await webSocketController.handleConnection(req, client: client)
+    }
+    
+    // WebSocket 연결을 처리하는 라우트
+    app.webSocket("testing") { request, client in
+        connectedClients.append(client)
+        
+        // 클라이언트가 연결될 때 호출
+        print("Client connected. Total connected clients: \(connectedClients.count)")
+        
+        // 클라이언트로부터 데이터를 수신할 때 호출
+        client.onBinary { client, data in
+            print("Received binary data of size: \(data.readableBytes)")
+            connectedClients
+                .filter { $0 !== client }
+                .forEach { $0.send(data) }
+        }
+        
+        // 클라이언트가 연결을 종료할 때 호출
+        client.onClose.whenComplete { _ in
+            connectedClients.removeAll { $0 === client }
+            print("Client disconnected. Total connected clients: \(connectedClients.count)")
+        }
     }
 }
