93c723aaab57942c3d94f9612376af2df75bef0f
youn9k <ace_lephant@naver.com>
2024-11-28 19:54:08 +0900

    feat/#128 :: 디코딩 에러 extension 추가
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>
    Co-Authored-By: seuhong <66902876+hsw1920@users.noreply.github.com>

--- PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ServiceImpl/SignalingServiceImpl.swift
+++ PhotoGether/DataLayer/PhotoGetherData/PhotoGetherData/ServiceImpl/SignalingServiceImpl.swift
@@ -64,7 +64,11 @@ extension SignalingServiceImpl {
         do {
             message = try self.decoder.decode(SignalingMessage.self, from: data)
         } catch {
-            debugPrint("수신한 메시지 decoding에 실패하였습니다.: \(error)")
+            if let error = error as? DecodingError {
+                debugPrint("수신한 메시지 decoding에 실패하였습니다.: \(error.fullDescription)")
+            } else {
+                debugPrint("수신한 메시지 decoding에 실패하였습니다.: \(error)")
+            }
             return
         }
         
@@ -76,6 +80,45 @@ extension SignalingServiceImpl {
         @unknown default:
             debugPrint("Unknown Message Type: \(message)")
             return
+        }
+    }
+}
+
+public extension DecodingError {
+    var fullDescription: String {
+        switch self {
+        case let .typeMismatch(type, context):
+            return """
+                    타입이 맞지 않습니다.\n
+                    \(type) 타입에서 오류 발생:\n
+                    codingPath: \(context.codingPath)\n
+                    debugDescription: \(context.debugDescription)\n
+                    underlyingError: \(context.underlyingError?.localizedDescription ?? "none")
+                    """
+        case let .valueNotFound(type, context):
+            return """
+                    값을 찾을 수 없습니다.\n
+                    \(type) 타입에서 오류 발생:\n
+                    codingPath: \(context.codingPath)\n
+                    debugDescription: \(context.debugDescription)\n
+                    underlyingError: \(context.underlyingError?.localizedDescription ?? "none")
+                    """
+        case let .keyNotFound(key, context):
+            return """
+                    키 \(key) 를 찾을 수 없습니다:\n
+                    codingPath: \(context.codingPath)\n
+                    debugDescription: \(context.debugDescription)\n
+                    underlyingError: \(context.underlyingError?.localizedDescription ?? "none")
+                    """
+        case let .dataCorrupted(context):
+            return """
+                    데이터 손실 에러입니다.\n
+                    codingPath: \(context.codingPath)\n
+                    debugDescription: \(context.debugDescription)\n
+                    underlyingError: \(context.underlyingError?.localizedDescription ?? "none")
+                    """
+        default:
+            return "알 수 없는 디코딩에 실패했습니다."
         }
     }
 }
