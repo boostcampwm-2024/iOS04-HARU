b7b7d7cb9cc4f0deaedc6398cfc7793d2a5801fa
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2024-11-29 23:05:09 +0900

    feat/#130 :: RemoteView 를 가져오는 유즈케이스 수정
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/GetRemoteVideoUseCaseImpl.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/GetRemoteVideoUseCaseImpl.swift
@@ -4,7 +4,15 @@ import PhotoGetherDomainInterface
 
 public final class GetRemoteVideoUseCaseImpl: GetRemoteVideoUseCase {
     public func execute() -> [UIView] {
-        connectionRepository.clients.map { $0.remoteVideoView }
+        let sortedClients = connectionRepository.clients
+            .sorted { lhs, rhs in
+                let lhsPosition = lhs.remoteUserInfo?.viewPosition.rawValue ?? Int.max
+                let rhsPosition = rhs.remoteUserInfo?.viewPosition.rawValue ?? Int.max
+                return lhsPosition < rhsPosition
+            }
+            .map { $0.remoteVideoView }
+        
+        return sortedClients
     }
     
     private let connectionRepository: ConnectionRepository
