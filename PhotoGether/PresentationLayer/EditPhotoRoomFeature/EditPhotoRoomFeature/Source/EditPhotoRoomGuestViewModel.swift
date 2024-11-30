e4e4a9fb16cce10de3a605924cac63eb23aec3cb
0Hooni <thddudgns972@gmail.com>
2024-11-30 20:59:26 +0900

    fix/#134 :: 의도하지 않은 시점에 스티커 unlock 발생 해결
    Co-Authored-By: seuhong <66902876+hsw1920@users.noreply.github.com>

--- PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EditPhotoRoomGuestViewModel.swift
+++ PhotoGether/PresentationLayer/EditPhotoRoomFeature/EditPhotoRoomFeature/Source/EditPhotoRoomGuestViewModel.swift
@@ -124,12 +124,12 @@ extension EditPhotoRoomGuestViewModel {
     private func handleDragSticker(sticker: StickerEntity, state: DragState) {
         // 소유권 -> OOO (확인)
         guard canInteractWithSticker(id: sticker.id) else { return }
-        unlockPreviousSticker()
         
         // Lock 획득 -> OXX
         // 로컬반영 -> XXO
         switch state {
         case .began:
+            unlockPreviousSticker()
             lockTappedSticker(id: sticker.id)
         case .changed:
             break
