51b242d2c96120f68f1fc50a81c24c78fd0d1ce9
youn9k <ace_lephant@naver.com>
2024-12-01 01:46:08 +0900

    feat/#137 :: 참가자 화면 뷰 바인딩
    컬렉션뷰에서 시간을 너무 많이 잡아먹어 우선 스택뷰로 구현했습니다.

--- PhotoGether/PresentationLayer/WaitingRoomFeature/WaitingRoomFeature/Source/View/WaitingRoomView.swift
+++ PhotoGether/PresentationLayer/WaitingRoomFeature/WaitingRoomFeature/Source/View/WaitingRoomView.swift
@@ -1,11 +1,28 @@
 import UIKit
 import DesignSystem
+import BaseFeature
+
+enum ParticipantPosition {
+    case topLeading
+    case topTrailing
+    case bottomLeading
+    case bottomTrailing
+}
 
 final class WaitingRoomView: UIView {
     let bottomBarView = UIView()
     let micButton = PTGMicButton(micState: .on)
     let linkButton = PTGCircleButton(type: .link)
     let startButton = PTGPrimaryButton()
+    
+    private(set) var topLeadingView = UIView()
+    private(set) var topTrailingView = UIView()
+    private(set) var bottomLeadingView = UIView()
+    private(set) var bottomTrailingView = UIView()
+    
+    private let vStackView = UIStackView()
+    private let topHStackView = UIStackView()
+    private let bottomHStackView = UIStackView()
     
     init() {
         super.init(frame: .zero)
@@ -17,6 +34,21 @@ final class WaitingRoomView: UIView {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateParticipantView(view: UIView, position: ParticipantPosition) {
+        switch position {
+        case .topLeading:
+            self.topLeadingView.addSubview(view)
+        case .topTrailing:
+            self.topTrailingView.addSubview(view)
+        case .bottomLeading:
+            self.bottomLeadingView.addSubview(view)
+        case .bottomTrailing:
+            self.bottomTrailingView.addSubview(view)
+        }
+        view.clipsToBounds = true
+        view.snp.makeConstraints { $0.edges.equalToSuperview() }
     }
     
     func toggleMicButtonState() {
@@ -31,13 +63,26 @@ final class WaitingRoomView: UIView {
 
 private extension WaitingRoomView {
     func addViews() {
-        [bottomBarView, micButton].forEach { addSubview($0) }
-        [linkButton, startButton].forEach {
-            bottomBarView.addSubview($0)
-        }
+        [bottomBarView, micButton, vStackView].forEach { addSubview($0) }
+        [linkButton, startButton].forEach { bottomBarView.addSubview($0) }
+        
+        [topLeadingView, topTrailingView].forEach { topHStackView.addArrangedSubview($0) }
+        [bottomLeadingView, bottomTrailingView].forEach { bottomHStackView.addArrangedSubview($0) }
+        [topHStackView, bottomHStackView].forEach { vStackView.addArrangedSubview($0) }
     }
     
     func setConstraints() {
+        let horizontalSpacing = Constants.sectionHorizontalSpacing * 2
+        let itemWidth = (UIScreen.main.bounds.width - horizontalSpacing - Constants.itemSpacing) / 2
+        let itemHeight = itemWidth * Constants.sizeMultiplier
+        let topOffset: CGFloat = APP_HEIGHT() > 667 ? 44 : 0 // 최소사이즈 기기 SE2 기준
+        
+        vStackView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(topOffset)
+            $0.horizontalEdges.equalToSuperview().inset(Constants.sectionHorizontalSpacing)
+            $0.bottom.equalTo(bottomBarView.snp.top)
+        }
+        
         bottomBarView.snp.makeConstraints {
             $0.height.equalTo(Constants.bottomBarViewHeight)
             $0.horizontalEdges.equalToSuperview()
@@ -70,6 +115,22 @@ private extension WaitingRoomView {
     func configureUI() {
         self.backgroundColor = PTGColor.gray90.color
         
+        vStackView.axis = .vertical
+        vStackView.spacing = Constants.itemSpacing
+        vStackView.distribution = .fillEqually
+        
+        [topHStackView, bottomHStackView].forEach {
+            $0.axis = .horizontal
+            $0.spacing = Constants.itemSpacing
+            $0.distribution = .fillEqually
+        }
+        
+        [topLeadingView,
+        topTrailingView,
+        bottomLeadingView,
+         bottomTrailingView
+        ].forEach { $0.backgroundColor = PTGColor.gray50.color }
+
         startButton.setTitle(to: StartButtonTitle.one.rawValue)
     }
 }
@@ -98,5 +159,9 @@ extension WaitingRoomView {
         static let circleButtonSize: CGSize = CGSize(width: 52, height: 52)
         static let startButtonHeight: CGFloat = 52
         static let micButtonBottomSpacing: CGFloat = -4
+        
+        static let sizeMultiplier: CGFloat = 290 / 179 // 피그마 디자인에 따른 세로/가로 비율입니다.
+        static let sectionHorizontalSpacing: CGFloat = 12
+        static let itemSpacing: CGFloat = 11
     }
 }
