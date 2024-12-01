c611147ef20921650b15b29760d4ea8cc4fbd75b
0Hooni <thddudgns972@gmail.com>
2024-12-01 17:56:22 +0900

    feat/#143 :: EmojiURL 프로퍼티 생성

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainInterface/Entity/EmojiEntity.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomainInterface/Entity/EmojiEntity.swift
@@ -4,7 +4,21 @@ public struct EmojiEntity: Decodable {
     public let emoji: String
     public let group: EmojiGroup
     public let annotation: String
-
+    public var emojiURL: URL? {
+        guard let emojiURL = URL(string: "https://www.emoji.family/api/emojis")
+        else { return nil }
+        
+        let style = emojiStyle.blobmoji.rawValue
+        let ext = "png"
+        let size = "96"
+        
+        return emojiURL
+            .appendingPathComponent(emoji)
+            .appendingPathComponent(style)
+            .appendingPathComponent(ext)
+            .appendingPathComponent(size)
+    }
+    
     public init(
         emoji: String,
         group: EmojiGroup,
@@ -13,6 +27,10 @@ public struct EmojiEntity: Decodable {
         self.emoji = emoji
         self.group = group
         self.annotation = annotation
+    }
+    
+    private enum emojiStyle: String {
+        case noto, twemoji, openmoji, blobmoji, fluent, fluentflat
     }
 }
 
