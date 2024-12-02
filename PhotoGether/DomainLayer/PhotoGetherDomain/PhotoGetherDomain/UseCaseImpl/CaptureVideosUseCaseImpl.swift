348f949643287e42948c40b0cb5387c43e93107c
hsw1920 <hsw1920@gmail.com>
2024-12-02 12:28:13 +0900

    refactor/#161 :: 강제 언래핑 제거 및 기본값 추가
    Co-Authored-By: YeongHoon Song <37678646+0Hooni@users.noreply.github.com>
    Co-Authored-By: Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
    Co-Authored-By: Youngkyu Song <ace_lephant@naver.com>

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/CaptureVideosUseCaseImpl.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/CaptureVideosUseCaseImpl.swift
@@ -1,12 +1,19 @@
 import UIKit
 import PhotoGetherDomainInterface
+import DesignSystem
 
 public final class CaptureVideosUseCaseImpl: CaptureVideosUseCase {
     public func execute() -> [UIImage] {
-        let localImage = [connectionRepository.capturedLocalVideo!]
+        guard let localImage = connectionRepository.capturedLocalVideo
+        else { return [
+            PTGImage.temp1.image,
+            PTGImage.temp2.image,
+            PTGImage.temp3.image,
+            PTGImage.temp4.image]
+        }
         let remoteImages = connectionRepository.clients.map { $0.captureVideo() }
         
-        return localImage + remoteImages
+        return [localImage] + remoteImages
     }
     
     private let connectionRepository: ConnectionRepository
