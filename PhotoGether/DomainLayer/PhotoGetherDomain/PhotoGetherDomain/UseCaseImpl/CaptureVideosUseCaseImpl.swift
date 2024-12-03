0b1bda52c713531e766617c593e9d0888ed1d4e8
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2024-12-03 17:52:55 +0900

    fix/#177 :: 사진 전달되는 순서 정렬해서 보내기
    - viewPosition에 따라 적당한 순서로 이미지 배열을 정렬해서 보냅니다.
    - PositionOrder는 참가자순서로 나열된 ViewPosition을 배열에 담길 순서로 바꿔줍니다
    - userInfo가 nil일 경우 PositionOder의 sequence를 2로 처리합니다.(배열의 2번 인덱스가 마지막 자리)

--- PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/CaptureVideosUseCaseImpl.swift
+++ PhotoGether/DomainLayer/PhotoGetherDomain/PhotoGetherDomain/UseCaseImpl/CaptureVideosUseCaseImpl.swift
@@ -11,14 +11,51 @@ public final class CaptureVideosUseCaseImpl: CaptureVideosUseCase {
             PTGImage.temp3.image,
             PTGImage.temp4.image]
         }
-        let remoteImages = connectionRepository.clients.map { $0.captureVideo() }
         
-        return [localImage] + remoteImages
+        let localUserImageInfo = [(connectionRepository.localUserInfo, localImage)]
+        let remoteUserImagesInfo = connectionRepository.clients
+            .map { ($0.remoteUserInfo, $0.captureVideo()) }
+        
+        return sortImages(localUserImageInfo + remoteUserImagesInfo)
     }
     
     private let connectionRepository: ConnectionRepository
     
     public init(connectionRepository: ConnectionRepository) {
         self.connectionRepository = connectionRepository
+    }
+    
+    private func sortImages(_ images: [(user: UserInfo?, image: UIImage)]) -> [UIImage] {
+        let convertedArray = images.map {
+            (position: PositionOder(rawValue: $0.user?.viewPosition.rawValue ?? -1),
+             image: $0.image)
+        }
+        
+        // 배열의 2번 인덱스가 마지막 자리이기 때문에 nil일 경우 2로 설정했습니다
+        let sortedByViewPosition = convertedArray.sorted {
+            let lhs = $0.position?.sequence ?? 2
+            let rhs = $1.position?.sequence ?? 2
+            return lhs < rhs
+        }
+        
+        return sortedByViewPosition.map { $0.image }
+    }
+}
+
+
+// case의 순서는 참가자의 참가 순서에 따른 화면 배치이고 sequence는 이미지 데이터 전달할 때의 배열 순서입니다
+private enum PositionOder: Int {
+    case topLeading
+    case bottomTrailing
+    case topTrailing
+    case bottomLeading
+    
+    var sequence: Int {
+        switch self {
+        case .topLeading: 0
+        case .topTrailing: 1
+        case .bottomLeading: 2
+        case .bottomTrailing: 3
+        }
     }
 }
