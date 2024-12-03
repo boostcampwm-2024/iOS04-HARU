ba4860953ad697dbb23ad3228f8dccb55b0f3554
youn9k <ace_lephant@naver.com>
2024-12-04 00:20:30 +0900

    feat/#175 :: 방은 한번만 생성할 수 있도록 처리

--- PhotoGether/PresentationLayer/WaitingRoomFeature/WaitingRoomFeature/Source/ViewModel/WaitingRoomViewModel.swift
+++ PhotoGether/PresentationLayer/WaitingRoomFeature/WaitingRoomFeature/Source/ViewModel/WaitingRoomViewModel.swift
@@ -29,6 +29,7 @@ public final class WaitingRoomViewModel {
     private let toggleLocalMicStateUseCase: ToggleLocalMicStateUseCase
     
     private var isHost: Bool
+    private var inviteLinkMessage: String?
     private var cancellables = Set<AnyCancellable>()
     private let output = PassthroughSubject<Output, Never>()
     
@@ -131,6 +132,10 @@ public final class WaitingRoomViewModel {
     }
     
     private func handleLinkButtonDidTap() {
+        if let inviteLinkMessage {
+            self.output.send(.shouldShowShareSheet(inviteLinkMessage))
+            return
+        }
         createRoomUseCase.execute()
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { [weak self] completion in
@@ -138,8 +143,9 @@ public final class WaitingRoomViewModel {
                     debugPrint(error.localizedDescription)
                     self?.output.send(.shouldShowToast("Failed to create room"))
                 }
-            }, receiveValue: { [weak self] roomLink in
-                self?.output.send(.shouldShowShareSheet(roomLink))
+            }, receiveValue: { [weak self] inviteLinkMessage in
+                self?.inviteLinkMessage = inviteLinkMessage
+                self?.output.send(.shouldShowShareSheet(inviteLinkMessage))
                 self?.output.send(.readyToStart)
             })
             .store(in: &cancellables)
